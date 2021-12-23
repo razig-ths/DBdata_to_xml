bdcfbfb7fd333af87cde0000fae2a535db3d3223
razig-ths <walid.rezig18@gmail.com>
2021-12-23 22:35:38 +0100

    db update

--- patient.sql
+++ patient.sql
@@ -1,8 +1,13 @@
-﻿/* Data for the 'patient' table  (Records 1 - 4) */
+﻿/* Data for the 'patient' table  (Records 1 - 9) */
 
 INSERT INTO `patient` (`id_patient`, `nom`, `prenom`, `age`, `sexe`) VALUES
 
-  (1, 'amir', 'chekhar', 21, 'homme'),
-  (2, 'abdelmalek', 'rezig', 24, 'hommme'),
-  (3, 'deffase', 'massoud', 23, 'homme'),
-  (4, 'taher', 'boutadjine', 22, 'homme');
+  (1, 'amir', 'chekhar', 21, 'Male'),
+  (2, 'abdelmalek', 'rezig', 24, 'Male'),
+  (3, 'deffase', 'massoud', 23, 'Male'),
+  (4, 'taher', 'boutadjine', 22, 'Male'),
+  (5, 'amina', 'amoucha', 30, 'Female'),
+  (6, 'ines', 'triek', 19, 'Female'),
+  (7, 'tarek', 'zelbah', 33, 'Male'),
+  (8, 'naama', 'boulaach', 33, 'Female'),
+  (9, 'walid', 'saber', 39, 'Male');
